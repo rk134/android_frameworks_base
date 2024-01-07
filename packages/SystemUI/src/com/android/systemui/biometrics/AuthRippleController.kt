91eb21d0574eff08e83d87b5d645ebea2f80d458
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-07 14:20:19 +0530

    SystemUI: Disable animations during biometric wake-and-unlock
    Animations slow down the phone. Gotta go fast especially while
    wake-and-unlocking with capacitive fingerprint scanners.
    Like @ jesec said in https://github.com/LineageOS/android_frameworks_base/commit/32244a3d0207df4bbd834fb64b3538752380dbb4,
    OEMs have been doing this for years.
    
    Based on:
    
    Author: Rishawn <70256146+Stealth1226@users.noreply.github.com>
    Date:   Mon Jan 10 09:47:15 2022 +0530
    
        SystemUI: Allow disabling ripple effect on unlock [1/2]
    
        neobuddy89:
        * Reverse logic. Enabled animation by default.
    
    Author: Nauval Rizky <enuma.alrizky@gmail.com>
    Date:   Wed Jan 19 08:31:56 2022 +0700
    
        SystemUI: Fix method for disabling unlock ripple animation
    
        NotificationShadeWindowController's are not gracefully closed, which
        leads to top parts of the screen is being obscured by invisible shade window.
        And also, "ripple" is actually still exists due to lightRevealScrim are
        being started by onKeyguardFadingAwayChanged, which is not what user expect
        when they want to disable the effect.
    
        So in order to handle those cases, lets intercept it from the
        AuthRippleController instead.
    
        [palaych] With system settings we need to pass UserHandle.USER_CURRENT to make sure we are getting proper value.
    
        Change-Id: I85ab96368c8b0099700f3de18ba95ce63eaac443
    
    Co-authored-by: Rishawn <70256146+Stealth1226@users.noreply.github.com>
    Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Change-Id: I12f152e2c62b22ec75bcf86591721de01ab39c70

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleController.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleController.kt
@@ -52,6 +52,7 @@ import com.android.systemui.statusbar.NotificationShadeWindowController
 import com.android.systemui.statusbar.commandline.Command
 import com.android.systemui.statusbar.commandline.CommandRegistry
 import com.android.systemui.statusbar.phone.BiometricUnlockController
+import com.android.systemui.statusbar.phone.BiometricUnlockController.MODE_WAKE_AND_UNLOCK
 import com.android.systemui.statusbar.policy.ConfigurationController
 import com.android.systemui.statusbar.policy.KeyguardStateController
 import com.android.systemui.util.ViewController
@@ -101,6 +102,9 @@ class AuthRippleController @Inject constructor(
 
     private var udfpsController: UdfpsController? = null
     private var udfpsRadius: Float = -1f
+
+    private val isRippleEnabled: Boolean
+        get() = biometricUnlockController.mode != MODE_WAKE_AND_UNLOCK
 
     override fun start() {
         init()
@@ -218,6 +222,8 @@ class AuthRippleController @Inject constructor(
     }
 
     private fun showUnlockedRipple() {
+        if (!isRippleEnabled) return
+
         notificationShadeWindowController.setForcePluginOpen(true, this)
 
         // This code path is not used if the KeyguardTransitionRepository is managing the light
@@ -242,6 +248,13 @@ class AuthRippleController @Inject constructor(
 
     override fun onKeyguardFadingAwayChanged() {
         if (lightRevealMigration()) {
+            return
+        }
+
+        if (!isRippleEnabled) {
+            // reset and hide the scrim so it doesn't appear on the next notification shade usage
+            lightRevealScrim.revealAmount = 1f
+            startLightRevealScrimOnKeyguardFadingAway = false
             return
         }
 
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -2794,6 +2794,9 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
 
         mScrimController.setExpansionAffectsAlpha(!unlocking);
 
+        mScrimController.setLaunchingAffordanceWithPreview(
+                mBiometricUnlockController.isWakeAndUnlock());
+
         if (mAlternateBouncerInteractor.isVisibleState()) {
             if (DeviceEntryUdfpsRefactor.isEnabled()) {
                 if ((!mKeyguardStateController.isOccluded() || mShadeSurface.isPanelExpanded())
